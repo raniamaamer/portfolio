11dcf23818eb70fd11a0bddee24d86b7bc064207
Rania Maamer <raniamaamer2001@gmail.com>
2026-07-16 13:04:39 +0100

    Ajout du CV en anglais avec bouton de sélection de langue

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -55,7 +55,7 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
+          <Document key={lang} file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
